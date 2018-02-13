2d95dee90b81b6e6e51a37e100aef50401d1d4fe
Sar <sar.haidar@gmail.com>
2018-02-13 10:37:12 +0200

    Converted daily_folder str to bytes

--- mitx_residential_etl.py
+++ mitx_residential_etl.py
@@ -96,7 +96,7 @@ def export_course(course_ids):
                                '/edx/app/edxapp/edx-platform/manage.py',
                                'lms', '--settings', 'aws',
                                'export_course', course_id,
-                               daily_folder + course_id + b'.tar.gz'], stdout=subprocess.PIPE) as proc:
+                               bytes(daily_folder, 'utf-8') + course_id + b'.tar.gz'], stdout=subprocess.PIPE) as proc:
             logger.info(proc.stdout.read())
 
 
